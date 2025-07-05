0a7350d22b9bb46214b3fe3c5d6c7cbaf9223d1e
SlayerOrnstein <6075693+SlayerOrnstein@users.noreply.github.com>
2025-07-05 09:30:07 -0400

    chore: fixed item config test
    doesn't look like whatever color I have on valk exist in items curently

--- test/unit/itemConfig.spec.ts
+++ test/unit/itemConfig.spec.ts
@@ -8,67 +8,65 @@ describe('ItemConfig', () => {
     it('should handle creating items when parsing data', () => {
       const config = {
         Skins: [
-          '/Lotus/Upgrades/Skins/Berserker/ValkyrDeluxeHelmet',
-          '/Lotus/Upgrades/Skins/Clan/ClanEmblemItem',
-          '',
-          '/Lotus/Upgrades/Skins/Berserker/BerserkerDeluxeNobleAnims',
+          '/Lotus/Upgrades/Skins/Dragon/ChromaPrimeHelmet',
           '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
-          '/Lotus/Upgrades/Skins/Berserker/ValkyrPrimeDangles',
+          '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
+          '/Lotus/Upgrades/Skins/Dragon/ChromaNobleAnims',
+          '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
+          '/Lotus/Upgrades/Skins/Dragon/ChromaPrimeSkin',
+          '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
+          '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
+          '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
+          '/Lotus/Upgrades/Skins/Armor/WarframeDefaults/EmptyCustomization',
           '',
           '',
           '',
           '',
+          '/Lotus/Upgrades/Skins/Effects/Kuva/KuvaFireEphemera',
+          '/Lotus/Upgrades/Skins/Voices/DefaultWarframeVoiceItem',
         ],
         pricol: {
-          t0: -2544384,
-          t1: -13881549,
+          t0: -13881549,
+          t1: -1644569,
           t2: -2544384,
           m0: -2544384,
-          m1: -11443299,
+          m1: -10126911,
           en: -2544384,
-          e1: -11443299,
-        },
-        sigcol: {
-          t0: -2131101191,
-          t1: -243302404,
-          t2: -2132934656,
-          en: -2130771329,
+          e1: -10126911,
         },
         attcol: {
-          t0: -2544384,
-          t1: -13881549,
+          t0: -13881549,
+          t1: -1644569,
           t2: -2544384,
-          t3: -2176318,
+          t3: -2773927,
           m0: -2544384,
           m1: -11443299,
           en: -2544384,
           e1: -11443299,
         },
         syancol: {
-          t0: -2544384,
-          t1: -13881549,
-          t2: -2544384,
-          t3: -2176318,
+          t0: -10461867,
+          t1: -1644569,
+          t2: -2228224,
+          t3: -2773927,
           m0: -2544384,
           m1: -11443299,
           en: -2544384,
           e1: -11443299,
         },
-        PvpUpgrades: [] as string[]
       };
 
       const { skins, primaryColor } = new ItemConfig(config);
 
       assert.strictEqual(
-        skins?.map((s) => s.uniqueName).includes('/Lotus/Upgrades/Skins/Berserker/ValkyrDeluxeHelmet'),
+        skins?.map((s) => s.uniqueName).includes('/Lotus/Upgrades/Skins/Dragon/ChromaPrimeHelmet'),
         true
       );
       assert.exists(primaryColor);
 
       const primary = primaryColor.primary;
       assert.exists(primary);
-      assert.strictEqual(primary.hex, '26D300');
-      assert.strictEqual(primary.isTransparent, false);
+      assert.strictEqual(primary.hex, 'D3D0CD');
       assert.isNotEmpty(primaryColor.emissive);
     });
   });
